6d9c39ac6096d5fe1a0f87b9b4b4d2f57a758c8d
sabbir20201 <sabbirsarkar674@gmail.com>
2023-11-08 23:22:18 +0600

    deleted a components

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -2,20 +2,20 @@
 const AboutUs = () => {
     return (
         <div className=" my-14">
-          
-        <div className="grid grid-cols-2 gap-5">
-            
-            <div>
-                <img src="https://i.ibb.co/gwH8tgv/istockphoto-1291318636-1024x1024.jpg" />
+
+            <div className="grid grid-cols-2 gap-5">
+
+                <div>
+                    <img src="https://i.ibb.co/gwH8tgv/istockphoto-1291318636-1024x1024.jpg" />
+                </div>
+                <div>
+                    <h1 className="text-4xl font-bold pb-5">Abots Us</h1>
+                    <p className=" mb-8">We are a leading online marketing agency, specializing in data-driven strategies that drive growth and success. Our team combines expertise in SEO, SEM, social media, and content marketing to help businesses thrive in the digital landscape.</p>
+                    <p>As a top-tier online marketing agency, we excel in delivering cutting-edge solutions for businesses. Our dedicated team harnesses the power of SEO, SEM, social media, and content marketing to optimize online presence, enhance brand visibility, and drive sustainable growth in the ever-evolving digital realm.</p>
+                    <button className="btn btn-primary mt-6">Read more</button>
+                </div>
+
             </div>
-            <div>
-            <h1 className="text-4xl font-bold pb-5">Abots Us</h1>
-            <p className=" mb-8">We are a leading online marketing agency, specializing in data-driven strategies that drive growth and success. Our team combines expertise in SEO, SEM, social media, and content marketing to help businesses thrive in the digital landscape.</p>
-            <p>As a top-tier online marketing agency, we excel in delivering cutting-edge solutions for businesses. Our dedicated team harnesses the power of SEO, SEM, social media, and content marketing to optimize online presence, enhance brand visibility, and drive sustainable growth in the ever-evolving digital realm.</p>
-            <button className="btn btn-primary mt-6">Read more</button>
-            </div>
-            
-        </div>
         </div>
     );
 };
--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            <img className='h-screen w-full relative' src='https://i.ibb.co/tDd0QYb/istockphoto-1443560890-1024x1024.jpg' />
-            <div className='absolute'>
-                <h1>Unlocking Online Success with Targeted Marketing</h1>
-                <p>Strategize, promote, and analyze campaigns for higher ROI in the dynamic world of online marketing</p>
+        <div className='relative'>
+            <div>
+                <img className='h-screen w-full relative'
+                 src='https://i.ibb.co/pw7YWGh/istockphoto-1494851346-1024x1024.jpg' />
+            </div>
+            <div className='absolute top-48 left-28'>
+                <h1 className='text-6xl font-bold max-w-3xl text-white'>Unlocking Online Success with Targeted Marketing</h1>
+                <p className='text-xl semi-bold max-w-3xl text-white pt-9'>Strategize, promote, and analyze campaigns for higher ROI in the dynamic world of online marketing</p>
             </div>
         </div>
     );
