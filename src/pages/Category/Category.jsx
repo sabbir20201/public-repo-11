2c61c7b11934b0d7cf6069a7145a63c36f9e703b
sabbir20201 <sabbirsarkar674@gmail.com>
2023-11-08 22:11:17 +0600

    deleted unnaccessary components

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -34,26 +34,49 @@ const Category = () => {
     }, [categoryDetails])
     return (
         <div className=' py-12'>
-    <h1 className='text-center text-4xl font-bold pb-10 '>BrowseByCategory </h1>
-    <div className=''>
+            <h1 className='text-center text-4xl font-bold pb-10 '>BrowseByCategory </h1>
+            <div className=''>
 
-            <Tabs>
-                <TabList>
-                    {
-                        categores.map(item => (
-                            <Tab key={item._id}>
-                                <Link> {item.category}</Link>
-                                <Link> {item.jobTitle}</Link>
-                            </Tab>
-                        ))
-                    }
+                <Tabs>
+                    <TabList>
+                        {
+                            categores.map(item => (
+                                <Tab key={item._id}>
+                                    <Link> {item.category}</Link>
+                                    <Link> {item.jobTitle}</Link>
+                                </Tab>
+                            ))
+                        }
 
-                </TabList>
-                <div>
+                    </TabList>
+                    <div>
+                        <TabPanel>
+                            <div className='grid grid-cols-3 gap-5'>
+                                {
+                                    filteredWebDesign.map(item => (
+                                        <div className="border border-black text-start bg-base-100">
+                                            <div className="card-body items-start text-start">
+                                                <p><span className="font-bold text-xl">Job Title :</span> <span className="text-xl font-semibold">{item.jobTitle}</span></p>
+                                                <p><span className="font-bold text-xl">Minimum Price :</span> <span className="text-xl font-semibold">{item.minimumPrice}</span></p>
+                                                <p><span className="font-bold text-xl">Deadline :</span> <span className="text-xl font-semibold">{item.deadline}</span></p>
+                                                <p><span className="font-bold text-xl">description :</span> <span className="text-xl font-semibold">{item.description}</span></p>
+                                                <div className="card-actions">
+                                                    <Link to={`/details/${item._id}`}>
+                                                        <button className="btn btn-primary">Bid Now</button>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                    </div>
+
                     <TabPanel>
                         <div className='grid grid-cols-3 gap-5'>
                             {
-                                filteredWebDesign.map(item => (
+                                filteredGraphicDesign.map(item => (
                                     <div className="border border-black text-start bg-base-100">
                                         <div className="card-body items-start text-start">
                                             <p><span className="font-bold text-xl">Job Title :</span> <span className="text-xl font-semibold">{item.jobTitle}</span></p>
@@ -61,7 +84,7 @@ const Category = () => {
                                             <p><span className="font-bold text-xl">Deadline :</span> <span className="text-xl font-semibold">{item.deadline}</span></p>
                                             <p><span className="font-bold text-xl">description :</span> <span className="text-xl font-semibold">{item.description}</span></p>
                                             <div className="card-actions">
-                                                <Link to={`/details/${item._id}`}>
+                                                <Link>
                                                     <button className="btn btn-primary">Bid Now</button>
                                                 </Link>
                                             </div>
@@ -71,58 +94,35 @@ const Category = () => {
                             }
                         </div>
                     </TabPanel>
-                </div>
 
-                <TabPanel>
-                    <div className='grid grid-cols-3 gap-5'>
-                        {
-                            filteredGraphicDesign.map(item => (
-                                <div className="border border-black text-start bg-base-100">
-                                    <div className="card-body items-start text-start">
-                                        <p><span className="font-bold text-xl">Job Title :</span> <span className="text-xl font-semibold">{item.jobTitle}</span></p>
-                                        <p><span className="font-bold text-xl">Minimum Price :</span> <span className="text-xl font-semibold">{item.minimumPrice}</span></p>
-                                        <p><span className="font-bold text-xl">Deadline :</span> <span className="text-xl font-semibold">{item.deadline}</span></p>
-                                        <p><span className="font-bold text-xl">description :</span> <span className="text-xl font-semibold">{item.description}</span></p>
-                                        <div className="card-actions">
-                                            <Link>
-                                                <button className="btn btn-primary">Bid Now</button>
-                                            </Link>
+
+                    <TabPanel>
+                        <div className='grid grid-cols-3 gap-5'>
+
+                            {
+                                filteredDigitalMarketing.map(item => (
+                                    <div className="border border-black text-start bg-base-100">
+                                        <div className="card-body items-start text-start">
+                                            <p><span className="font-bold text-xl">Job Title :</span> <span className="text-xl font-semibold">{item.jobTitle}</span></p>
+                                            <p><span className="font-bold text-xl">Minimum Price :</span> <span className="text-xl font-semibold">{item.minimumPrice}</span></p>
+                                            <p><span className="font-bold text-xl">Deadline :</span> <span className="text-xl font-semibold">{item.deadline}</span></p>
+                                            <p><span className="font-bold text-xl">description :</span> <span className="text-xl font-semibold">{item.description}</span></p>
+                                            <div className="card-actions">
+                                                <Link>
+                                                    <button className="btn btn-primary">Bid Now</button>
+                                                </Link>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </TabPanel>
+                                ))
+                            }
+                        </div>
+                    </TabPanel>
 
-
-                <TabPanel>
-                    <div className='grid grid-cols-3 gap-5'>
-
-                        {
-                            filteredDigitalMarketing.map(item => (
-                                <div className="border border-black text-start bg-base-100">
-                                    <div className="card-body items-start text-start">
-                                        <p><span className="font-bold text-xl">Job Title :</span> <span className="text-xl font-semibold">{item.jobTitle}</span></p>
-                                        <p><span className="font-bold text-xl">Minimum Price :</span> <span className="text-xl font-semibold">{item.minimumPrice}</span></p>
-                                        <p><span className="font-bold text-xl">Deadline :</span> <span className="text-xl font-semibold">{item.deadline}</span></p>
-                                        <p><span className="font-bold text-xl">description :</span> <span className="text-xl font-semibold">{item.description}</span></p>
-                                        <div className="card-actions">
-                                            <Link>
-                                                <button className="btn btn-primary">Bid Now</button>
-                                            </Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </TabPanel>
-              
-            </Tabs>
+                </Tabs>
             </div>
-            </div>
-       
+        </div>
+
     );
 };
 
